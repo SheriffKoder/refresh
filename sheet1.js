694c50d84882d2a204b280f37cede39c2e865b48
SheriffKoder <sheriff.koder@hotmail.com>
2022-12-09 06:09:15 +0200

    Recap and add points

--- sheet1.js
+++ sheet1.js
@@ -76,16 +76,44 @@
     
     //use '' when inside in html events when outer is " "
 
+    //Objects
+    person[myDataName] = myDataValue;
+    Obj.["two letter"]
+    Obj.[name+ "extension"]
+
+    =transfer structured data
+    alert(key in user);   //true as key exists in user
+    Let obj = { [promptfruit]: 4 };	alert(obj.apple);
+    For (let x in obj)
+    Obj in obj
+
+    //Object constructors
+    function Toy (sound) {
+        this.soundnew = sound;
+        this.quacknew = fnPara or function () {
+                        console.log(this.sound);
+                    }
+    }
+    var Duck = new Toy ("quack"); // new copy of Toy is named accessed by the name Duck
+    console.log (typeof toy); //object !!
+    console.log (Duck instanceof Toy); //true
+    Duck.soundnew //quack
+    Var Toys = [Duck, Car];
+
+
+    
+
 
     label:loop , break label; //apply on parent loop
-    continue;(next loop) break; //out of current loop
+    continue;(next loop) 
+    break; //out of current loop
     
     
     for (const indexname in ArrayName ){
          objectName[indexname];
     }
     
-    0 is not '0'
+    0 is false not '0' the string
     
     
     if ( numX.length !=8|| isNaN(numX.slice(0,3)) || numX.charAt(3)!="-"  || isNaN(numX.slice(4,8))      ) {
@@ -107,6 +135,10 @@
     
     //Arrow
     let fun = (para) => {expression};
+    const comment = comments.find(comment => comment.id === 823423); 
+    //Return without return
+    //understand what it says if comment.id === return true
+
     
     //declaration
     function fun () {
@@ -118,6 +150,15 @@
     
     }
     
+
+    //return anonymous objects
+    Return { name, age, }
+    Return {name: , age: }
+
+    //return an anonymous function/anon-obj+method that uses an arg 
+    //and returns something like a function or comparison
+    //outside use returnedObj.method()
+
     
     
     
@@ -131,7 +172,8 @@
     */
     
     // console.log/warn/info/error/assert/clear/dir/count/time(it takes)/table
-    
+    //console.table( object ); //neat object representation
+
     
     let func = (arg1, arg2, ... ) = > expression;
     
@@ -146,12 +188,14 @@
     //fun(new_func) { newFunc(2)    //pass function to function
     //let newFunc = functionName (Declaration or expression)
     
-    ?? > const foo = if_null_or_undefined ?? "return_this"
+    ?? > const foo = null ?? "return_this";
+    //if_null_or_undefined, return string
 
     
     
-    //divisible i%3 === 0
-    
+    //divisible by 3 ? 
+    //i%3 === 0
+    //remainder
     
     function min (a,b) {
         //return (a > b)? a : b;
@@ -195,30 +239,83 @@
     array.concat(a2,string1);
     
     array.unshift/shift();	//add/remove from beginning
-    array.push/pop();,
+    array.push/pop(); //add/remove from end
     
     - array matrix =[ [,,][,,][,,]  ];
     - arrays can hold function, obj-ref, 
 
-    array.sort();
+    - array.sort( () => num1-num2 );
+
+    links.map(link => link.textContent).filter(place => place.includes("Wall");
+
+
+    array.findIndex(fn); //fn returns true on p se value //where
+    array.find(fn); //fn returns true on p se value //returns 1st item found
+    array.every(fn); //fn returns true on p matches cond. //all items match cond
+    Array.some(fn); //fn returns true on p matches cond //at least one item 
+
+    array.sort(fn) //fn passes over array and takes two paras, returns 1/-1 if greater/smaller
+
+    array.reduce(fn, {}); //fn has loop initials p, item p - initial object //returns the initials after passing though all items
+
+    array.filter(fn); //fn returns items that return true o meeting condition to a new array
+    initials[item] //give value if not true,/found //inc this specific item
     
+    Const newComments = [ ...method1, ...method2]; 
+    //add two array outputs into one array //spread
+    Const links = [...x.querySelectorAll("a")]; 
+    //every item into an array
+    Const [aLast, aFirst] = lastOne.split(", ") 
+    //return in tow arrays rather than one, useful to spearate
+    
+
     
     }
     
     /*////////////////////////////////////////////////////////////////////*/
     Element Methods
     { 
-    setAttribute
-    getAttribute
-    getElementbyId
+
+    //document.body acts as target mode when nothing in particular has focus
+    document.body...
+    console.dir(document) //the doc properties
+    console.dir(document.all[30) //all elements
+    console.dir(document.links) //array of links
+
+    //div2611.setAttribute("id", "idName")
+    //div2611.getAttribute("id")
+    //div2611.removeAttribute("id")
+    const div2611 = document.createElement("div"); //allocates memory to be appended later in the DOM
     getElementbyClassName
     getByTagName
     .querySelectorAll
     Element.style.left
     Element.src 
+    x.href
+    x.alt
     x.textContent
-    //loop take event action on tag name array 
+    //div2611.innerHTML
+
+
     
+    //loop take event action on tag name array 
+    //Array.From used on query not elementsByTag
+
+    //classes
+    x.className = "class1 class2";
+    //div2611.classList.add("new");
+    //div2611.classList.remove("new");
+    //div2611.classList.toggle("new");
+
+    //styles
+    div2611.style.color = "blue";
+    div2611.style.cssText = "color: blue;   background: white;" ;  //add several styles
+    div2611.setAttribute("style", "color: blue;   background: white;");
+
+    //div2611.style.backgroundColor //to access b-c use camelCase and remove -
+    //div2611.style["background-color"] //or use in brackets
+
+
     x =parseInt(prompt()); //input into number
     let x = confirm(text); //like prompt but ok(true)/cancel(false)
     
@@ -226,6 +323,8 @@
     
     
     .setTimeout(fn, ms, argument_optional)
+    clearTimeout(returned_setTimeout)
+
     let x = elem.setInterval(fn, ms); clearInterval(x);
     
     ////Events
@@ -242,7 +341,8 @@
     e.keyCode == "65";
     if (e.key == "v" && e.ctrlKey)
     e.propertyName
-    
+    consol.log(e.altKey) //output true if event+altpressed
+
     
     
     //conversion 
@@ -279,6 +379,8 @@
     .includes()
     .endsWith()
     .indexOf() //.lastindexOf();
+    itemName.indexOf(text) != -1  //indexOf return
+
     .search()
     
     .replace(), .replaceAll()
@@ -286,6 +388,9 @@
     
     .fromChartoCode()
     .valueOf()
+
+    .toLowerCase()	//to default inputs for processing
+
     
     string.method1().method2();, //obj.m.m
     
@@ -294,16 +399,16 @@
     
     
     //////// number methods 
+    {
     Math.trunc(x) //removes the characters or floats from a number
     
     //Number(x.toFixed(1)); .123 => .1 
     //Math.round( x * 10) / 10
-
+    }
     
     //////// Window events
     {
     
-    .onload
     .innerWidth
     .onhashchange  //window.location.hash = ;
     .onresize
@@ -313,19 +418,49 @@
     elem.requestFullscreen();
     .fullscreenchange .fullscreenerror
     
+    body.ononline
+    body.onoffline
+
+    .onload/.onunload
+    .onpagehide //like unload but allows cashing
+    .onpageshow //like load but runs even when cash 
+    //e.persisted returns false if not cached
+
+//    console.time('fetching data');
+//    console.timeEnd('fetching data'); //calc time taken for a function
+
+
+
     
     }
     
     //////// Events
     {
+
     
-    - events, track of clicks, page scroll, touch quantity, 
-    - e.preventDefault();   //prevent default behavior, ex link not open but does another thing
+    console.lo(e) //show all properties 
+    e.type //type of event
+    
+    
+    events, track of clicks, page scroll, touch quantity
+    e.preventDefault();   //prevent default behavior, ex link not open but does another thing
 
-
+    //position and placing
     x = target.clientX; .clientY; //px position map.onclick = fn();
-    - screenX user's screen, pageX browser's screen
+    - screenX users screen, pageX browsers screen
     //  pageX //mouseclick to draw
+    E.offsetX/Y //position from the element itself
+    //Rgb values based on offsetX
+    .offsetWidth //size/position of an element, read only
+    .offsetLeft //ret left position relative to parent
+    .clientTop //ret width of top border
+    .offsetParent //ret nearest ancestor
+    .clientHeight/Width //size/position of a child in element with padding but ignores borders
+    rect = x.getBoundingClientRect();	  //return elements position on screen
+    rect.top/bottom/right/left.toFixed();
+    window.scrollBy(100,100);   //force scroll
+    window.pageXOffset, window.pageYOffset //add pointer position
+
 
     
     x = new Date().toString, getfullyear, getDay //Date sub-methods
@@ -333,7 +468,7 @@
     .onDrop, onDragOver .onDrag .onLeave onDragEnter
     .onPlay
     
-    .invalid (with html required)
+    .invalid (with html required attribute)
     .valid
     .input
     .change
@@ -346,8 +481,26 @@
     .onkeypress //not work with alt, cmd etc
     .onclick
     .contextmenu    //rightclick
-    .oncopy/oncut
+    .oncopy/oncut/onpaste
     .dblclick
+
+
+
+    .onmouseup/down
+    .onmouseenter/leave (div only)
+    .onmouseover/out (propagate inner element fires too)
+    .onmousemove	(px move event)
+    .onwheel (scroll/zoom out/in)
+
+    .onscroll
+
+    .ontouchstart	
+    .ontouchmove	
+    .ontouchend	
+
+    e.touches.length
+    let {pageX, pageY} = e.touches[i];
+
     
     
     .animationstart
@@ -356,6 +509,10 @@
     myDiv.style.animation 
 
     .transitionend  //end of css transition
+
+    //hmtl Form/input-submit, 
+    form on submit, e.preventDefault(); //avoid refreshing
+
     
     
     }
@@ -380,6 +537,8 @@
     .onloadmetadata (audio/video info)
     .onloadstart (start looking for the file)
 
+    audio.currentTime =0; //end
+
 
 
     }
@@ -390,6 +549,7 @@
 
     .querySelectorAll("div[attribute=data-name"); //where data-Name, name is a custom attribute 
     //multi attribute, different numbers
+    item.querySelctior from its children
 
 
     .parentNode
@@ -404,7 +564,28 @@
     .hasChildNodes(x); 
     .removeChild();
 
+    If (e.target.classList.contains("delete"))
+    itemList.removeChild(e.target.parentElement);
+    
+    .replaceChild 	//removes
+    .insertBefore(x,x); //to insert in Dom after creation and appending
 
+    Node.TEXT_NODE  , node.nodeValue//holds the string text 
+    Node.COMMENT_NODE
+    Node.ELEMENT_NODE
 
+    Li.appendChild(document.createTextNode(valueVar));
 
+    //childNodes,firstChild, nextSibling  (incl text/space nodes)
+    //children, first/LastElementChild (not incl text/space)
+
+    console.dir(container2611.firstElementChild);
+    console.dir(container2611.previousElementSibling);
+        
+    //parentNode.appendChild(childNode);    //append to last of children
+    //parentNode.insertBefore(newNode, referenceNode);  //append before reference node in parent node
+    //parentNode.removeChild(childname)
+
+    .children //only elements
+    item.firstChild.textContent
 
